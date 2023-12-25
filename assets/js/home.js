5014ea3e3a5b155dca210758deeb0ec8e256abbd
Mamedova Kubra <kiubrajm@code.edu.az>
2023-12-25 14:16:48 +0400

    finished product section

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,20 +1,72 @@
 "use strict"
 
-$(function(){
+$(function () {
 
-    $('.slider-for').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        fade: true,
-        asNavFor: '.slider-nav'
-      });
-      $('.slider-nav').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        asNavFor: '.slider-for',
-        dots: true,
-        centerMode: true,
-        focusOnSelect: true
-      });
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true
+  });
+
+
+
+
+  let products = document.querySelectorAll(".product")
+
+  products.forEach(productImage => {
+    productImage.addEventListener("mouseover", function () {
+      this.children[1].classList.remove("d-none")
+    })
+
+  })
+
+  products.forEach(productImage => {
+    productImage.addEventListener("mouseout", function () {
+      this.children[1].classList.add("d-none")
+    })
+
+  })
+
+
+
+  products.forEach(productIcon => {
+    productIcon.addEventListener("mouseover", function () {
+      this.children[2].classList.remove("d-none")
+    })
+
+  })
+
+  products.forEach(productIcon => {
+    productIcon.addEventListener("mouseout", function () {
+      this.children[2].classList.add("d-none")
+    })
+
+  })
+
+
+  let heartIcons = document.querySelectorAll(".product .icons .heart-icon");
+  
+  heartIcons.forEach(heartIcon => {
+    heartIcon.addEventListener("click", function () {
+
+      this.nextElementSibling.classList.remove("d-none")
+
+      this.nextElementSibling.addEventListener("click", function () {
+        this.classList.add("d-none")
+      })
+
+    })
+
+  });
+
 })
