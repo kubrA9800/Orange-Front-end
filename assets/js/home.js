2926f2b73e1ea33cf7e7aa673fa8848fe0615ca1
Mamedova Kubra <kiubrajm@code.edu.az>
2023-12-26 18:17:21 +0400

    added sidebar

--- assets/js/home.js
+++ assets/js/home.js
@@ -55,7 +55,7 @@ $(function () {
 
 
   let heartIcons = document.querySelectorAll(".product .icons .heart-icon");
-  
+
   heartIcons.forEach(heartIcon => {
     heartIcon.addEventListener("click", function () {
 
@@ -69,4 +69,40 @@ $(function () {
 
   });
 
+
+  let openSidebarIcon = document.querySelector(".header-all .hamburger-icon")
+  let sidebar = document.querySelector(".sidebar")
+  let closeSidebarIcon = document.querySelector(".sidebar .close-icon")
+  let overlay = document.querySelector(".overlay")
+
+  openSidebarIcon.addEventListener("click", function () {
+    sidebar.classList.remove("move-sidebar")
+    overlay.style.display = "block";
+
+  })
+
+  closeSidebarIcon.addEventListener("click", function () {
+    sidebar.classList.add("move-sidebar")
+    overlay.style.display = "none"
+  })
+
+  overlay.addEventListener("click", function(){
+    sidebar.classList.add("move-sidebar")
+    this.style.display = "none";
+    seacrhSidebar.classList.add("move-sidebar")
+})
+
+
+  let seacrhSidebar=document.querySelector(".search-sidebar")
+  let searchIcon=document.querySelector(".icons .search-icon")
+  let closeSearchIcon=document.querySelector(".search-sidebar .fa-xmark")
+  searchIcon.addEventListener("click", function () {
+    seacrhSidebar.classList.remove("move-sidebar")
+    overlay.style.display = "block";
+
+  })
+  closeSearchIcon.addEventListener("click", function () {
+    seacrhSidebar.classList.add("move-sidebar")
+    overlay.style.display = "none"
+  })
 })
