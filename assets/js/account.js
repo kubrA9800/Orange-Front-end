e7b67c4e1cd016ed4738517a37b4473ac1fb9023
Mamedova Kubra <kiubrajm@code.edu.az>
2023-12-29 13:00:09 +0400

    finished account page

--- assets/js/account.js
+++ assets/js/account.js
@@ -13,4 +13,24 @@ let inputs=document.querySelectorAll(".input input")
         this.previousElementSibling.style.top="5px"
         this.previousElementSibling.style.fontSize="16px"
     }
+    
  })
+
+
+ let links=document.querySelectorAll("#login-register .link a")
+ links.forEach(link=>
+  
+    link.onclick=function(){
+       
+        let id=this.getAttribute("data-id")
+        let boxs= document.querySelectorAll(".box")
+        for(const box of boxs){
+            
+            if( box.getAttribute("data-id")===id){
+                box.classList.remove("d-none")
+            }else{
+                box.classList.add("d-none")
+
+            }
+        }
+    })
