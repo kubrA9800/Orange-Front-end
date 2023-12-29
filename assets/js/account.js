d721bdf8952cc7968a0a7e1d6484bf861f54bdd5
Mamedova Kubra <kiubrajm@code.edu.az>
2023-12-29 19:31:13 +0400

    added product detail

--- assets/js/account.js
+++ assets/js/account.js
@@ -1,34 +1,42 @@
 "use strict"
+let placeholders = document.querySelectorAll(".form .input span")
 
-let inputs=document.querySelectorAll(".input input")
- inputs.forEach(input=>{
-    input.onfocus=function(){
-        this.previousElementSibling.style.top="-14px"
-        this.previousElementSibling.style.fontSize="14px"
+placeholders.forEach(placeholder =>
+    placeholder.onclick = function () {
+        this.nextElementSibling.focus()
+    })
+
+
+let inputs = document.querySelectorAll(".input input")
+inputs.forEach(input => {
+    input.onfocus = function () {
+        this.previousElementSibling.style.top = "-14px"
+        this.previousElementSibling.style.fontSize = "14px"
     }
- })
+})
 
- inputs.forEach(input=>{
-    input.onblur=function(){
-        this.previousElementSibling.style.top="5px"
-        this.previousElementSibling.style.fontSize="16px"
+inputs.forEach(input => {
+    input.onblur = function () {
+        this.previousElementSibling.style.top = "5px"
+        this.previousElementSibling.style.fontSize = "16px"
     }
-    
- })
+
+})
 
 
- let links=document.querySelectorAll("#login-register .link a")
- links.forEach(link=>
-  
-    link.onclick=function(){
-       
-        let id=this.getAttribute("data-id")
-        let boxs= document.querySelectorAll(".box")
-        for(const box of boxs){
-            
-            if( box.getAttribute("data-id")===id){
+
+let links = document.querySelectorAll("#login-register .link a")
+links.forEach(link =>
+
+    link.onclick = function () {
+
+        let id = this.getAttribute("data-id")
+        let boxs = document.querySelectorAll(".box")
+        for (const box of boxs) {
+
+            if (box.getAttribute("data-id") === id) {
                 box.classList.remove("d-none")
-            }else{
+            } else {
                 box.classList.add("d-none")
 
             }
